34681f302defce5fc220f03288b989c408fa17b5
João Vitor Novaes <jvn.novaes@edu.unifil.br>
2026-03-02 21:29:35 -0300

    Add project archives
    Adicionado os arquivos necesasrios para o desenvolvimento do projeto de Listas simples e Listas Dinamicas por meio da implementação.

---
+++ ListaDinamica.java
@@ -0,0 +1,51 @@
+public class ListaDinamica implements ListaOperacoes{
+    @Override
+    public int removerTodas(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int contar() {
+        return 0;
+    }
+
+    @Override
+    public int adicionarVarios(String[] elementos) {
+        return 0;
+    }
+
+    @Override
+    public String obter(int indice) {
+        return "";
+    }
+
+    @Override
+    public boolean inserir(int indice, String elemento) {
+        return false;
+    }
+
+    @Override
+    public String removerPorIndice(int indice) {
+        return "";
+    }
+
+    @Override
+    public void limpar() {
+
+    }
+
+    @Override
+    public int ultimoIndiceDe(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int contarOcorrencias(String elemento) {
+        return 0;
+    }
+
+    @Override
+    public int substituir(String antigo, String novo) {
+        return 0;
+    }
+}
